52080e76e14e49829823d6f1a8f17fc987c0dcd2
Strokachuk Oleg <zifter.ai@gmail.com>
2012-04-17 14:36:43 +0300

    Added GNU GPL licinse

--- include/Graph/Graph.h
+++ include/Graph/Graph.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+ **
+ ** GESloth - Graph Editor Sloth
+ ** For the latest info, see https://github.com/zifter/GESloth
+ **
+ ** Copyright (c) 2012 zifter
+ **
+ ** GESloth free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** GESloth is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+
 /*
  * Graph.h
  *
@@ -66,7 +88,11 @@ public:
 	//! Remove graph
 	bool remove( Graph* gr );
 
+	//! Clean all graph
 	void removeAll();
+
+	//! Delete all edges, which doesn't have source and dest Node in this graph
+	void fixEdge();
 private:
 	//! Лист вершин
 	QList<Node*> mNodes;
--- src/Gui/GESScene.cpp
+++ src/Gui/GESScene.cpp
@@ -1,3 +1,25 @@
+/****************************************************************************
+ **
+ ** GESloth - Graph Editor Sloth
+ ** For the latest info, see https://github.com/zifter/GESloth
+ **
+ ** Copyright (c) 2012 zifter
+ **
+ ** GESloth free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** GESloth is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+
 #include <QContextMenuEvent>
 #include <QMenu>
 #include <QApplication>
@@ -223,7 +245,9 @@ void GESScene::copyObj() {
 
 	QMimeData *mData = new QMimeData;
 	GESFileWriter writer;
-	QByteArray* bt = &writer.writeToByte( toGraph( Buf ) );
+	Graph* graph = toGraph( Buf );
+	graph->fixEdge();
+	QByteArray* bt = &writer.writeToByte( graph );
 	mData->setData("Graph Editor Sloth/items", (*bt));
 	QClipboard *pastebuf = QApplication::clipboard();
 	pastebuf->setMimeData(mData);
